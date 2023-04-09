52eb3df726b9fcbdb4e2ff4573e7b0d0ce046c29
Shantanu2k19 <shantanusingh2030@gmail.com>
2023-04-09 07:30:14 +0530

    audio added

--- sidebar/src/index.js
+++ sidebar/src/index.js
@@ -5,14 +5,17 @@ import Modal from './modal'
 import App from './App';
 import Test from './test';
 import Final from './final';
-  
+import Audio from "./audio";
+import "./audio.css"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
         {/* <Test /> */}
         
-        <Modal />
+        {/* <Modal /> */}
+        <Audio />
         
         {/* <App /> */}
     </>
